670c207bd8c47525991e2e652a1577c12dfad797
Hidayt Rahman <hidaytrahman@gmail.com>
2022-02-13 20:39:53 +0530

    feat(theme): added initial dark theme

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import { theme } from "core/theme";
+import { theme, darkTheme } from "core/theme";
 import { ThemeProvider } from "styled-components";
 import Profile from "./components/profile/Profile";
+import GlobalStyle from "components/styled/GlobalStyle"
 
 function App() {
   return (
     <main className="App">
       <ThemeProvider theme={theme}>
+      <GlobalStyle />
         <Profile />
       </ThemeProvider>
     </main>
