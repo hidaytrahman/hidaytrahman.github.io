2fc894a298cff4dd09e5bd47b9d15c9393a7648d
Hidayt Rahman <hidaytrahman@gmail.com>
2023-09-10 01:33:39 +0530

    feat: update me json

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -1,64 +1,59 @@
-import Typography from "components/common/Typography";
-import { Box, Container, Flex, Skill } from "components/styled/Core.styled";
-import { applyColor, colors } from "core/utils";
+import Typography from 'components/common/Typography';
+import { Box, Container, Flex, Skill } from 'components/styled/Core.styled';
+import { applyColor, colors } from 'core/utils';
 
-const Skills = ({profile}) => {
+const Skills = ({ profile }) => {
+	return (
+		<Container padding='2rem'>
+			<Flex>
+				<Box>
+					<Typography variant='h3' margin='5px 0'>
+						Skillset
+					</Typography>
+					<Flex wrap='wrap'>
+						<Box>
+							{profile?.skills?.primary &&
+								profile?.skills?.primary.map((skill, index) => (
+									<Skill
+										src={`https://img.shields.io/badge/-${skill.title}-${applyColor(
+											colors,
+											index
+										)}?style=flat&logo=${skill.title.toLocaleLowerCase()}`}
+										key={index}
+									/>
+								))}
+						</Box>
 
-    return <Container padding="2rem">
-    <Flex>
-      <Box>
-        <Typography variant="h3" margin="5px 0">
-          Skillset
-        </Typography>
-        <Flex wrap="wrap">
-          <Box>
-            {profile?.skills?.primary &&
-              profile?.skills?.primary.map((skill, index) => (
-                <Skill
-                  src={`https://img.shields.io/badge/-${
-                    skill.title
-                  }-${applyColor(
-                    colors,
-                    index
-                  )}?style=flat&logo=${skill.title.toLocaleLowerCase()}`}
-                  key={index}
-                />
-              ))}
-          </Box>
+						<Box>
+							{profile?.skills?.frameworks &&
+								profile?.skills?.frameworks.map((skill, index) => (
+									<Skill
+										src={`https://img.shields.io/badge/-${skill.title}-${applyColor(
+											colors,
+											index
+										)}?style=flat&logo=${skill.title.toLocaleLowerCase()}`}
+										key={index}
+									/>
+								))}
+						</Box>
 
-          <Box>
-            {profile?.skills?.frameworks &&
-              profile?.skills?.frameworks.map((skill, index) => (
-                <Skill
-                  src={`https://img.shields.io/badge/-${
-                    skill.title
-                  }-${applyColor(
-                    colors,
-                    index
-                  )}?style=flat&logo=${skill.title.toLocaleLowerCase()}`}
-                  key={index}
-                />
-              ))}
-          </Box>
-
-          <Box>
-            {profile?.skills?.secondary &&
-              profile?.skills?.secondary.map((skill, index) => (
-                <Skill
-                  src={`https://img.shields.io/badge/-${
-                    skill.title
-                  }-${applyColor(
-                    colors,
-                    index + 2
-                  )}?style=flat&logo=${skill.title.toLocaleLowerCase()}`}
-                  key={index}
-                />
-              ))}
-          </Box>
-        </Flex>
-      </Box>
-    </Flex>
-  </Container>
-}
+						<Box>
+							{profile?.skills?.secondary &&
+								profile?.skills?.secondary.map((skill, index) => (
+									<Skill
+										src={`https://img.shields.io/badge/-${skill.title}-${applyColor(
+											colors,
+											index + 2
+										)}?style=flat&logo=${skill.title.toLocaleLowerCase()}`}
+										key={index}
+									/>
+								))}
+						</Box>
+					</Flex>
+				</Box>
+			</Flex>
+		</Container>
+	);
+};
 
 export default Skills;
