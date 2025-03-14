8f19b9ee3f0793c205c75bd324a66d9e59b214a3
Hidayt Rahman <hidaytrahman@gmail.com>
2025-03-14 14:50:41 +0530

    revert section

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,6 +14,8 @@ import {
 	Box,
 	ProfileCard,
 	StoryContainer,
+	Divider,
+	Link,
 } from './styled/Core.styled';
 import { greetNow } from 'core/utils';
 import { STORY_MODES } from 'core/config';
@@ -119,13 +121,67 @@ const Header = ({ profile, openModal }) => {
 									</StoryContainer>
 								) : null}
 							</Box>
-							<Box>
-								<Typography variant='h4'>{greetNow()}</Typography>
-								<Typography variant='h2'>{profile.name}</Typography>
-								<Typography variant='h5'>{profile.bio}</Typography>
-								{/* <Button onClick={() => setIsResumeOpen(true)}>My Resume</Button> */}
+							
+							<Box width='300px' margin='10px' padding='20px'>
+								<Typography variant='h1'>{profile.name}</Typography>
+								<Typography variant='body1' margin='5px 0'>
+									💼 {profile?.organization?.designation} at{' '}
+									<Link href={profile?.organization?.url} target='_blank' rel='noopener noreferrer'>
+										{profile?.organization?.title}
+									</Link>
+								</Typography>
+
+								<Link
+									href='https://www.google.com/maps/place/Delhi/@28.6468931,76.9528367,11z/data=!3m1!4b1!4m5!3m4!1s0x390cfd5b347eb62d:0x37205b715389640!8m2!3d28.7040592!4d77.1024902'
+									target='_blank'
+								>
+									<Typography variant='body2' margin='5px 0'>
+										📍 {profile.location} 🇮🇳
+									</Typography>
+								</Link>
+								<Typography variant='body2' margin='5px 0'>
+									🗨️󠁄󠁄 Language:{' '}
+									{profile.personal?.languages?.map(({ title }, index) => (
+										<span key={title}>
+											{title}
+											{index === profile.personal?.languages.length - 1 ? ' ' : ', '}
+										</span>
+									))}
+								</Typography>
+
+								<Divider />
+								<Typography variant='body2'> {profile.bio}</Typography>
 							</Box>
 						</ProfileCard>
+						
+						{/* SECOND SECTION */}
+						<Box margin='10px' padding='20px'>
+							<Typography variant='h3'>Hi 👋 {greetNow()}</Typography>
+							<br />
+							<Typography variant='body1'>
+								{profile.greet?.replace('{{%totalExperience%}}', profile.totalExperience)}
+							</Typography>
+							<Typography variant='body1' margin='10px 0'>
+								{profile.intro}
+							</Typography>
+							<Typography variant='body1'>"{profile.quotes}"</Typography>
+							<br />
+							<Flex gap='15px' alignItems='center'>
+								<Button
+									variant='secondary'
+									onClick={() => window.open('https://www.linkedin.com/in/hidaytrahman/', '_blank')}
+								>
+									Say Hi
+								</Button>
+								<Typography variant='body1'>Or</Typography>
+								<Button
+									variant='primary'
+									onClick={() => window.open('https://calendly.com/hidaytrahman/15', '_blank')}
+								>
+									Book my calender
+								</Button>
+							</Flex>
+						</Box>
 					</Flex>
 				</Container>
 			</StyledHeader>
